ef65413bb28211fdb2f47140aeecc82c9fe228b4
Gwen <gwenaelle.batta@studetn.hepl.be>
2022-02-03 11:03:31 +0100

    init

---
+++ main.php
@@ -0,0 +1,20 @@
+<?php
+?>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Capitales</title>
+</head>
+<body>
+    <h1>Choisis un pays, je te donnerai sa capitale</h1>
+    <form action="main.php">
+        <select name="country" id="country">
+            <option value=""></option>
+        </select>
+    </form>
+</body>
+</html>
